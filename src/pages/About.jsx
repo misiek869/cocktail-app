7f587010e59369a62e9656c0eb366317d8fa80ea
Michal Gawik <gawlik.zator@o2.pl>
2024-05-30 16:55:42 +0200

    style about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/About'
 
 const About = () => {
-	return <div>About</div>
+	return (
+		<Wrapper>
+			<h3>About Us</h3>
+			<p>
+				Let me introduce "Cocktail App". The ultimate party sidekick app that
+				fetches cocktails from the Cocktails DB API. With a flick of your
+				finger, you can unlock a treasure trove of enchanting drink recipes that
+				will make your taste buds dance and your friends jump with joy. Get
+				ready to shake up your mixology game, one fantastical mocktail at a
+				time, and let the laughter and giggles flow!
+			</p>
+		</Wrapper>
+	)
 }
 
 export default About
