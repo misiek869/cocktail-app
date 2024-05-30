1df591ab8efcdf2f7f38f995caa7493e042f244e
Michal Gawik <gawlik.zator@o2.pl>
2024-05-30 14:52:15 +0200

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <h2>home page</h2>,
+	},
+	{
+		path: 'about',
+		element: <h3>about:)</h3>,
+	},
+])
 
 function App() {
-	return <></>
+	return <RouterProvider router={router}></RouterProvider>
 }
 
 export default App
