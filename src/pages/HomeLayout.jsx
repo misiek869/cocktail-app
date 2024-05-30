948d601426cc3ce4f3bda4db504d5b9e93ae044e
Michal Gawik <gawlik.zator@o2.pl>
2024-05-30 15:22:37 +0200

    setup routes and outlet

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const HomeLayout = () => {
 	return (
 		<div>
 			<h1>Homelayout</h1>
-			<Link to='/about'>About</Link>
+			<Outlet />
 		</div>
 	)
 }
