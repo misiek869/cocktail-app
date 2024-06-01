6810f3b21e83e82ca102b80cd6004294909ea74f
Michal Gawik <gawlik.zator@o2.pl>
2024-06-01 14:25:42 +0200

    create cocktail card and list, create styles

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/CocktailCard'
+import { Link } from 'react-router-dom'
 
-const CocktailCard = () => {
-	return <div>Card</div>
+const CocktailCard = ({ image, name, info, id, glass }) => {
+	return (
+		<Wrapper>
+			<div className='img-container'>
+				<img className='img' src={image} alt={name} />
+			</div>
+			<div className='footer'>
+				<h4>{name}</h4>
+				<h5>{glass}</h5>
+				<p>{info}</p>
+				<Link className='btn' to={`/cocktail/${id}`}>
+					details
+				</Link>
+			</div>
+		</Wrapper>
+	)
 }
 
 export default CocktailCard
