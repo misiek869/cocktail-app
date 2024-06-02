226864a734d497e673a8bb953e190ccbb2f3d6df
Michal Gawik <gawlik.zator@o2.pl>
2024-06-02 16:41:58 +0200

    set up newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,50 @@
 import React from 'react'
 
 const Newsletter = () => {
-	return <div>Newsletter</div>
+	return (
+		<form className='form'>
+			<h4 style={{ textAlign: 'center', marginBottom: '2rem' }}></h4>
+			{/* name */}
+			<div className='form-row'>
+				<label className='form-label' htmlFor='name'>
+					Name
+				</label>
+				<input
+					className='form-input'
+					type='text'
+					name='name'
+					id='name'
+					defaultValue='john'
+				/>
+			</div>
+			{/* last name */}
+			<div className='form-row'>
+				<label className='form-label' htmlFor='lastName'>
+					Last name
+				</label>
+				<input
+					className='form-input'
+					type='text'
+					name='lastName'
+					id='lastName'
+					defaultValue='smith'
+				/>
+			</div>
+			{/* email */}
+			<div className='form-row'>
+				<label className='form-label' htmlFor='email'>
+					Email
+				</label>
+				<input
+					className='form-input'
+					type='email'
+					name='email'
+					id='email'
+					defaultValue='smith'
+				/>
+			</div>
+		</form>
+	)
 }
 
 export default Newsletter
