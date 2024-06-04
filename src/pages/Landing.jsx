f38835cdc06d4fb980d02e9a03755a0ec30a99a3
Michal Gawik <gawlik.zator@o2.pl>
2024-06-04 08:54:54 +0200

    fetch data with react query

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,18 +7,25 @@ import { useQuery } from '@tanstack/react-query'
 const cocktailSearchUrl =
 	'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 
+const searchDrinksQery = searchTerm => {
+	return {
+		queryKey: ['search', searchTerm || 'all'],
+		queryFn: async () => {
+			const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
+			return response.data.drinks
+		},
+	}
+}
+
 export const loader = async ({ request }) => {
 	const url = new URL(request.url)
-
 	const searchTerm = url.searchParams.get('search')
-
-	const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`)
-	return { drinks: response.data.drinks, searchTerm }
+	return { searchTerm }
 }
 
 const Landing = () => {
-	const { drinks, searchTerm } = useLoaderData()
-
+	const { searchTerm } = useLoaderData()
+	const { data: drinks } = useQuery(searchDrinksQery(searchTerm))
 	return (
 		<>
 			<SearchForm searchTerm={searchTerm} />
